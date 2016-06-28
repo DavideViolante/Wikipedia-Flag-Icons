b1845b58994c50cd85145159699dd2ebc67cdad2
dave <fungtq3@gmail.com>
2016-06-28 13:06:53 +0200

    Add new flags

--- Userscript/wikipediaFlagIcons.user.js
+++ Userscript/wikipediaFlagIcons.user.js
@@ -27,8 +27,11 @@
 		en: "usgb", de: "de", es: "es", fr: "fr", it: "it", nl: "nl", ja: "jp", pl: "pl", ru: "ru", sv: "se",
 		vi: "vn", id: "id", ms: "my", cs: "cz", ko: "kr", hu: "hu", no: "no", pt: "pt", ro: "ro", sr: "rs",
 		fi: "fi", tr: "tr", uk: "ua", zh: "cn", bs: "ba", bg: "bg", da: "dk", et: "ee", el: "gr", he: "il",
-		hr: "hr", lv: "lv", lt: "lt", nn: "no", sk: "sk", sl: "si", th: "th",
-		hi: "in", ca: "catalonia", simple: "gb", eo: "eo"
+		ge: "ge", uz: "uz", am: "et", arz: "eg", az: "az", be: "by", bi: "vu", bn: "bd", hy: "am", is: "is",
+		hr: "hr", lv: "lv", lt: "lt", sk: "sk", sl: "si", th: "th", ga: "ie", ka: "ge", kk: "kz", ne: "np",
+		kl: "gl", km: "kh", ky: "kg", lb: "lu", lo: "la", mg: "mg", mk: "mk", mn: "mn", mt: "mt", ps: "af",
+		rw: "rw", si: "lk", sm: "ws", so: "so", sq: "al", tg: "tj", tk: "tm", ur: "pk", uz: "uz", 
+		hi: "in", ca: "catalonia", eo: "eo", sco: "scotland", tet: "tl"
 	};
 
 	for(var prop in flagLangCode) {
