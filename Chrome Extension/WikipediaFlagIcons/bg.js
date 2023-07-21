2b696c728d494bfb0eda28d6369bfb5c3c5a83f6
DavideViolante <fungtq3@gmail.com>
2023-07-21 14:32:04 +0200

    Prepare for v3 version

--- Chrome Extension/WikipediaFlagIcons/bg.js
+++
@@ -1,5 +0,0 @@
-﻿chrome.browserAction.onClicked.addListener(function(activeTab) {
-  chrome.tabs.create({ 'url': 'options.html' });
-});
-
-chrome.storage.sync.set({ primaryLang: 'en', secondaryLang: 'en' }, function() {});
